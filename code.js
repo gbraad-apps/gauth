09b648a5ec423003ea2997444fed3c498d411fe9
Gerard Braad <me@gbraad.nl>
2012-06-08 17:11:21 +0800

    First version

---
+++ code.js
@@ -0,0 +1,92 @@
+function dec2hex(s) {
+    return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
+}
+
+function hex2dec(s) {
+    return parseInt(s, 16);
+}
+
+function base32tohex(base32) {
+    var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+    var bits = "";
+    var hex = "";
+
+    for (var i = 0; i < base32.length; i++) {
+        var val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+        bits += leftpad(val.toString(2), 5, '0');
+    }
+
+    // leftpad bits with 0 until length is a multiple of 4
+    while (bits.length % 4 != 0) {
+        bits = "0" + bits;
+    }
+
+    for (var i = bits.length - 4; i >= 0; i = i - 4) {
+        var chunk = bits.substr(i, 4);
+        hex = parseInt(chunk, 2).toString(16) + hex;
+    }
+    return hex;
+}
+
+function leftpad(str, len, pad) {
+    if (len + 1 >= str.length) {
+        str = Array(len + 1 - str.length).join(pad) + str;
+    }
+    return str;
+}
+
+function updateOtp(secret) {
+    var key = base32tohex(secret);
+    var epoch = Math.round(new Date().getTime() / 1000.0);
+    var time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+
+    var hmacObj = new jsSHA(time, "HEX");
+    var hmac = hmacObj.getHMAC(key, "HEX", "SHA-1", "HEX");
+
+    if (hmac != 'KEY MUST BE IN BYTE INCREMENTS') {
+        var offset = hex2dec(hmac.substring(hmac.length - 1));
+        // Debug
+        //var part1 = hmac.substr(0, offset * 2);
+        //var part2 = hmac.substr(offset * 2, 8);
+        //var part3 = hmac.substr(offset * 2 + 8, hmac.length - offset);
+    }
+
+    var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+    return otp = (otp).substr(otp.length - 6, 6);
+}
+
+function timer() {
+    var epoch = Math.round(new Date().getTime() / 1000.0);
+    var countDown = 30 - (epoch % 30);
+    if (epoch % 30 == 0) {
+        $('#otp').text(updateOtp(localStorage.keySecret));
+    }
+    $('#updatingIn').text(countDown);
+}
+
+$(function () {
+    // Check if local storage is supported
+    if (typeof (Storage) !== "undefined") {
+        // Set the overview page
+        $('#otp').text(updateOtp(localStorage.keySecret));
+        $('#account').text(localStorage.keyAccount);
+        setInterval(timer, 1000);
+
+        // Load from local storage
+        $('#keySecret').val(localStorage.keySecret);
+        $('#keyAccount').val(localStorage.keyAccount);
+    } else {
+        $('#updatingIn').text("x");
+        $('#account').text("No Storage support");
+    }
+
+    $('#save').click(function () {
+        // Save in local storage
+        localStorage.keySecret = $('#keySecret').val();
+        localStorage.keyAccount = $('#keyAccount').val();
+
+        // Set the overview page
+        $('#otp').text(updateOtp(localStorage.keySecret));
+        $('#account').text(localStorage.keyAccount);
+    });
+});
