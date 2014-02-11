6624622bc54aab694935fe7b2a17c033c2c372dd
Gerard Braad <me@gbraad.nl>
2014-02-11 16:02:56 +0800

    Changed to gulp as build system (used to be grunt)

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var log = require('gulp-util').log;
+
+var jshint = require('gulp-jshint');
+
+var codeFiles = ['js/**/*.js', '!node_modules/**'];
+
+gulp.task('lint', function(){
+  log('Linting Files');
+  return gulp.src(codeFiles)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter());
+});
+
+gulp.task('watch', function(){
+  log('Watching Files');
+  gulp.watch(codeFiles, ['lint']);
+});
+
+gulp.task('default', ['lint']);
