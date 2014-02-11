e41c28b73eb36bd65bd06370692cb25ade5ce2da
Gerard Braad <me@gbraad.nl>
2014-02-11 17:06:33 +0800

    Added livereload enabled server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+ 
 var gulp = require('gulp');
 var log = require('gulp-util').log;
 
@@ -17,6 +21,31 @@ gulp.task('lint', function(){
 gulp.task('watch', function(){
   log('Watching Files');
   gulp.watch(codeFiles, ['lint']);
+});
+
+function startExpress() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')());
+  app.use(express.static(EXPRESS_ROOT));
+  app.listen(EXPRESS_PORT);
+}
+
+var lr;
+function startLivereload() {
+  lr = require('tiny-lr')();
+  lr.listen(LIVERELOAD_PORT);
+}
+ 
+function notifyLivereload(event) {
+  gulp.src(event.path, {read: false})
+      .pipe(require('gulp-livereload')(lr));
+}
+
+gulp.task('server', function () {
+  startExpress();
+  startLivereload();
+  gulp.watch(['*.html', 'js/**/*.js'], notifyLivereload);
 });
 
 gulp.task('default', ['lint']);
