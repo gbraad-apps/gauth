03dc80d58a659795df6ae3fa33e36bc6defc30ab
multiwebinc <github@multiwebinc.com>
2015-01-08 17:22:17 -0600

    Many changes
    Rewrite using flat theme, get rid of jquery-mobile, add jquery-ui and bootstrap, change localStorage to chrome.storage when possible.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 // Main function
-$(document).on('pagecreate', '#main', function() {
-	// Background styling for dialogs
-	$('div[data-role="dialog"]').live('pagebeforeshow', function(e, ui) {
-		ui.prevPage.addClass("ui-dialog-background");
-	});
+$( document ).ready(function() {
 
-	$('div[data-role="dialog"]').live('pagehide', function(e, ui) {
-		$(".ui-dialog-background ").removeClass("ui-dialog-background");
-	});
+    // Use exports from locally defined module
+    var keysController = new gauth.KeysController();
+    keysController.init();
 
-	// Use exports from locally defined module
-	var keysController = new gauth.KeysController();
-	keysController.init();
+    var dialogOpts = {
+        autoOpen: false,
+        modal: true,
+        minWidth: 450,
+        show: 500,
+        hide: 500
+    };
+
+    $( "#about" ).dialog( dialogOpts );
+    $( "#addAccount" ).dialog( dialogOpts );
+    $( "#deleteAccount" ).dialog( dialogOpts );
+
+    $(".about").click(function() {
+        $( "#about" ).dialog("open");
+    });
+    $(".add-account").click(function() {
+        $( "#addAccount" ).dialog("open");
+    });
+    $(".cancel").click(function() {
+        $( "#addAccount, #deleteAccount, #about" ).dialog("close");
+    });
+    $("#delete").click(function() {
+        var index = $("#deleteAccount").attr("data-account-id");
+        keysController.deleteAccount(index);
+        $("#deleteAccount").dialog("close")
+    });
+
+    $(".jqueryui-tooltip").tooltip();
 });
+
