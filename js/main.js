a0374058b602d7a0c131e14038a4199953724964
Gerard Braad <me@gbraad.nl>
2015-01-12 10:26:28 +0800

    Update jquery, jquery mobile and style

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 // Main function
 $(document).on('pagecreate', '#main', function() {
-	// Background styling for dialogs
-	$('div[data-role="dialog"]').live('pagebeforeshow', function(e, ui) {
-		ui.prevPage.addClass("ui-dialog-background");
-	});
-
-	$('div[data-role="dialog"]').live('pagehide', function(e, ui) {
-		$(".ui-dialog-background ").removeClass("ui-dialog-background");
-	});
-
 	// Use exports from locally defined module
 	var keysController = new gauth.KeysController();
 	keysController.init();
