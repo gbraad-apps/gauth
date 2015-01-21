f68d09edbe37709b3bfb8ea353de715290e475c6
Gerard Braad <me@gbraad.nl>
2015-01-21 15:46:04 +0800

    Add port and ip address configuration to server.js
    This will introduce support to deploy to OpenShift

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 'use strict';
 
 // get port from environment settings for deployment on Heroku
-var EXPRESS_PORT = process.env.PORT || 4000;
+var EXPRESS_PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4000;
+var EXPRESS_IPADDR = process.env.OPENSHIFT_NODEJS_IP || process.env.IPADDR || '127.0.0.1';
 var EXPRESS_ROOT = __dirname;
- 
-function startExpress(root, port) {
+
+function startExpress(root, port, ipaddr) {
   var express = require('express');
   var app = express();
   app.use(express.static(root));
-  app.listen(port, function() {
-    console.log('Listening on port:', port);
+  app.listen(port, ipaddr, function() {
+    console.log('Listening on %s:%d',
+		ipaddr, port);
   });
 }
 
-startExpress(EXPRESS_ROOT, EXPRESS_PORT);
+startExpress(EXPRESS_ROOT, EXPRESS_PORT, EXPRESS_IPADDR);
