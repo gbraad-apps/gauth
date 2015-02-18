599afc74a251825874b6aeb47d650246557a7917
Jim Riggs <jim@riggs.me>
2015-02-18 13:54:13 -0600

    fix Travis decimal complaint

--- js/gauth.js
+++ js/gauth.js
@@ -248,7 +248,7 @@
             }
             $('#updatingIn').text(countDown);
             if (countDown <= 10) {
-                $('ul#accounts li a').css('color', '#b00').fadeTo(100, 1).fadeTo(500, .5);
+                $('ul#accounts li a').css('color', '#b00').fadeTo(100, 1).fadeTo(500, 0.5);
             }
         };
 
