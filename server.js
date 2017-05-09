0b9a54c4f73ee434bebdfabf0d1d390f38d9f481
Gerard Braad <me@gbraad.nl>
2017-05-09 12:15:35 +0800

    Remove OpenShift port assignment, default to 8080

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // get port from environment settings for deployment on Heroku
-var EXPRESS_PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4000;
-var EXPRESS_IPADDR = process.env.OPENSHIFT_NODEJS_IP || process.env.IPADDR || '127.0.0.1';
+var EXPRESS_PORT = process.env.PORT || 8080;
+var EXPRESS_IPADDR = process.env.IPADDR || '127.0.0.1';
 var EXPRESS_ROOT = __dirname;
 
 function startExpress(root, port, ipaddr) {
