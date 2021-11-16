38f964a774b1f24b8024657fd73592b0d5ae1040
Gerard Braad <me@gbraad.nl>
2021-11-16 12:38:20 +0000

    Sanitize account name (print as entered)

--- js/gauth.js
+++ js/gauth.js
@@ -178,7 +178,7 @@
                 var key = keyUtilities.generate(account.secret);
 
                 // Construct HTML
-                var detLink = $('<span class="secret"><h3>' + key + '</h3><p>' + account.name + '</p></span>');
+                var detLink = $('<span class="secret"><h3>' + key + '</h3>' + $('<p>').text(account.name).html() + '</span>');
                 var accElem = $('<li data-icon="false">').append(detLink);
  
                 if(editingEnabled) {
